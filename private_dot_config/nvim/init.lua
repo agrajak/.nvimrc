01cdb5696df7ff0b78fa0596f419d22779454468
agrajak <korbots@gmail.com>
2024-04-27 17:01:37 +0900

    hi

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -30,7 +30,13 @@ require("lazy").setup({
 	{
 		"kyazdani42/nvim-tree.lua",
 		config = function()
-			require("nvim-tree").setup()
+			require("nvim-tree").setup({
+				update_cwd = true,
+				update_focused_file = {
+					enable = true,
+					update_cwd = true,
+				},
+			})
 		end,
 	},
 	{
@@ -47,10 +53,10 @@ require("lazy").setup({
 			},
 			formatters_by_ft = {
 				lua = { "stylua" },
+				["*"] = { "prettierd", "prettier" },
 				-- Conform will run multiple formatters sequentially
 				python = { "isort", "black" },
 				-- Use a sub-list to run only the first available formatter
-				javascript = { { "prettierd", "prettier" } },
 			},
 		},
 	},
@@ -109,6 +115,66 @@ require("lazy").setup({
 	{
 		"chentoast/marks.nvim",
 	},
+	{ "folke/neodev.nvim", opts = {} },
+	{
+		"nvimdev/dashboard-nvim",
+		event = "VimEnter",
+		config = function()
+			require("dashboard").setup({
+				-- config
+			})
+		end,
+		dependencies = { { "nvim-tree/nvim-web-devicons" } },
+	},
+	{
+		"notjedi/nvim-rooter.lua",
+	},
+	{
+		"NeogitOrg/neogit",
+		dependencies = {
+			"nvim-lua/plenary.nvim", -- required
+			"sindrets/diffview.nvim", -- optional - Diff integration
+
+			-- Only one of these is needed, not both.
+			"nvim-telescope/telescope.nvim", -- optional
+		},
+		config = true,
+	},
+	{
+		"NStefan002/screenkey.nvim",
+		cmd = "Screenkey",
+		version = "*",
+		config = true,
+	},
+	{
+
+		"nvim-treesitter/nvim-treesitter",
+	},
+	{
+		"folke/trouble.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+		},
+	},
+	{
+		"romgrk/barbar.nvim",
+		dependencies = {
+			"lewis6991/gitsigns.nvim", -- OPTIONAL: for git status
+			"nvim-tree/nvim-web-devicons", -- OPTIONAL: for file icons
+		},
+		init = function()
+			vim.g.barbar_auto_setup = false
+		end,
+		opts = {
+			-- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
+			-- animation = true,
+			-- insert_at_start = true,
+			-- …etc.
+		},
+		version = "^1.0.0", -- optional: only update when a new 1.x version is released
+	},
 })
 
 require("leap").create_default_mappings()
@@ -117,11 +183,23 @@ vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+vim.keymap.set("n", "<leader>fr", builtin.lsp_references, {})
+vim.keymap.set("n", "<leader>fic", builtin.lsp_incoming_calls, {})
+vim.keymap.set("n", "<leader>foc", builtin.lsp_outgoing_calls, {})
+vim.keymap.set("n", "<leader>fds", builtin.lsp_document_symbols, {})
 
 require("mason").setup()
+require("mason-lspconfig").setup({
+	ensure_installed = { "tsserver", "lua_ls" },
+})
+-- checkout https://github.com/williamboman/mason-lspconfig.nvim?tab=readme-ov-file#configuration
+require("lspconfig").tsserver.setup({})
+require("lspconfig").lua_ls.setup({})
+
 require("lualine").setup()
 require("telescope").setup({ defaults = { file_ignore_patterns = { "node_modules" } } })
 require("command-completion").setup()
 require("marks").setup()
+require("nvim-rooter").setup()
 
 vim.cmd([[colorscheme tokyonight]])
