d78ca03b35016b62caaf85723fb643dec71fff3a
agrajak <korbots@gmail.com>
2024-04-27 15:44:51 +0900

    add plugins

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -10,19 +10,19 @@ vim.api.nvim_create_user_command("Format", function(args)
 	require("conform").format({ async = true, lsp_fallback = true, range = range })
 end, { range = true })
 
-
--- vim.api.nvim_set_keymap('n', '<D-b>', ':NvimTreeToggle', {})
+vim.wo.number = true
+vim.api.nvim_set_keymap("n", "<D-b>", ":NvimTreeToggle", {})
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -56,23 +56,72 @@ require("lazy").setup({
 	},
 	{
 		"williamboman/mason.nvim",
+		"williamboman/mason-lspconfig.nvim",
+		"neovim/nvim-lspconfig",
 	},
 	{
 		"neoclide/coc.nvim",
 		build = "yarn install --frozen-lockfile",
 	},
 	{
-  "folke/which-key.nvim",
-  event = "VeryLazy",
-  init = function()
-    vim.o.timeout = true
-    vim.o.timeoutlen = 300
-  end,
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-  }
-  }
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.6",
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {},
+	},
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		init = function()
+			vim.o.timeout = true
+			vim.o.timeoutlen = 300
+		end,
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		},
+	},
+	{
+		"ggandor/leap.nvim",
+	},
+	{
+		"kylechui/nvim-surround",
+		version = "*", -- Use for stability; omit to use `main` branch for the latest features
+		event = "VeryLazy",
+		config = function()
+			require("nvim-surround").setup({
+				-- Configuration here, or leave empty to use defaults
+			})
+		end,
+	},
+	{
+		"sindrets/diffview.nvim",
+	},
+	{
+		"smolck/command-completion.nvim",
+	},
+	{
+		"chentoast/marks.nvim",
+	},
 })
 
+require("leap").create_default_mappings()
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+
+require("mason").setup()
+require("lualine").setup()
+require("telescope").setup({ defaults = { file_ignore_patterns = { "node_modules" } } })
+require("command-completion").setup()
+require("marks").setup()
+
+vim.cmd([[colorscheme tokyonight]])
